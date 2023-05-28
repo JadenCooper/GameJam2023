99c6a1b72556e0738fc0862591f87beaffa8a0b7
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-05-28 14:19:26 +1200

    Add stuff to room manager

--- GameJam/Assets/Scripts/Managers/RoomManager.cs
+++ GameJam/Assets/Scripts/Managers/RoomManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RoomManager : MonoBehaviour
 {
     public List<Stairs> ListOfStairs = new List<Stairs>();
-    private List<GameObject> ListOfAllRooms = new List<GameObject>();
+    public List<GameObject> ListOfAllRooms = new List<GameObject>();
     private GameObject BossRoom;
     private List<GameObject> ListOfChosenRooms = new List<GameObject>();
     private List<GameObject> ListOfEnemies = new List<GameObject>();
@@ -14,53 +14,63 @@ public class RoomManager : MonoBehaviour
     private GameObject player;
     private void Start()
     {
-        player = GameObject.FindWithTag("Player");
-        StartRoom(CurrentRoom.GetComponent<RoomData>());
+        foreach (GameObject room in ListOfAllRooms)
+        {
+            room.SetActive(false);
+        }
+        ListOfAllRooms[0].SetActive(true);
+        //player = GameObject.FindWithTag("Player");
+        //StartRoom(CurrentRoom.GetComponent<RoomData>());
     }
+
     public void NextRoom()
     {
-        ListOfEnemies.Clear();
-        Destroy(CurrentRoom);
-        currentRoomIndex = currentRoomIndex + 1;
-        CurrentRoom = ListOfChosenRooms[currentRoomIndex];
-        Instantiate(CurrentRoom);
-        StartRoom(CurrentRoom.GetComponent<RoomData>());
+
+        //ListOfEnemies.Clear();
+        //Destroy(CurrentRoom);
+        ListOfAllRooms[currentRoomIndex].SetActive(false);
+        currentRoomIndex++;
+        player.transform.position = ListOfAllRooms[currentRoomIndex].GetComponent<RoomData>().PlayerSpawnLocation.transform.position;
+        ListOfAllRooms[currentRoomIndex].SetActive(true);
+        //CurrentRoom = ListOfChosenRooms[currentRoomIndex];
+        //Instantiate(CurrentRoom);
+        //StartRoom(CurrentRoom.GetComponent<RoomData>());
     }
 
-    public void StartRoom(RoomData roomData)
-    {
-        // Spawn Enemies
-        int spawnedAmmount = Random.Range((int)roomData.MinMaxEnemyAmount.x, (int)roomData.MinMaxEnemyAmount.y);
-        int i = 0;
-        Debug.Log(spawnedAmmount);
-        do
-        {
-            int enemyType = Random.Range(0, roomData.EnemyTypes.Count);
-            Instantiate(roomData.EnemyTypes[enemyType], roomData.EnemySpawnLocations[i]);
-            spawnedAmmount--;
-            i++;
-            if (i > roomData.EnemySpawnLocations.Count)
-            {
-                i = 0;
-            }
-        }while(spawnedAmmount > 0);
+    //public void StartRoom(RoomData roomData)
+    //{
+    //    // Spawn Enemies
+    //    int spawnedAmmount = Random.Range((int)roomData.MinMaxEnemyAmount.x, (int)roomData.MinMaxEnemyAmount.y);
+    //    int i = 0;
+    //    Debug.Log(spawnedAmmount);
+    //    do
+    //    {
+    //        int enemyType = Random.Range(0, roomData.EnemyTypes.Count);
+    //        Instantiate(roomData.EnemyTypes[enemyType], roomData.EnemySpawnLocations[i]);
+    //        spawnedAmmount--;
+    //        i++;
+    //        if (i > roomData.EnemySpawnLocations.Count)
+    //        {
+    //            i = 0;
+    //        }
+    //    }while(spawnedAmmount > 0);
 
-        player.transform.position = roomData.PlayerSpawnLocation.transform.position;
-    }
+    //    player.transform.position = roomData.PlayerSpawnLocation.transform.position;
+    //}
 
-    public void RandomizeRoom()
-    {
-        int roomAmount = Random.Range(3, ListOfAllRooms.Count);
-        int i = 0;
-        do
-        {
-            GameObject newRoom = ListOfAllRooms[Random.Range(0, ListOfAllRooms.Count)];
-            if (!ListOfChosenRooms.Contains(newRoom))
-            {
-                ListOfChosenRooms.Add(newRoom);
-                i++;
-            }
-        } while (i < roomAmount);
-        ListOfChosenRooms.Add(BossRoom);
-    }
+    //public void RandomizeRoom()
+    //{
+    //    int roomAmount = Random.Range(3, ListOfAllRooms.Count);
+    //    int i = 0;
+    //    do
+    //    {
+    //        GameObject newRoom = ListOfAllRooms[Random.Range(0, ListOfAllRooms.Count)];
+    //        if (!ListOfChosenRooms.Contains(newRoom))
+    //        {
+    //            ListOfChosenRooms.Add(newRoom);
+    //            i++;
+    //        }
+    //    } while (i < roomAmount);
+    //    ListOfChosenRooms.Add(BossRoom);
+    //}
 }
