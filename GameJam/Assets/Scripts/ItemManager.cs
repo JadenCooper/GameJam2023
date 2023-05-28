5cd83f712f1ab838bd4a2938e69deea34aa4903e
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-05-28 19:39:54 +1200

    Add Item Name Popup

--- GameJam/Assets/Scripts/ItemManager.cs
+++ GameJam/Assets/Scripts/ItemManager.cs
@@ -34,37 +34,39 @@ public class ItemManager : MonoBehaviour
     { 
         items.Add(newItem);
         playerStats.ItemChanged(newItem);
-        PlayItemPopup(newItem);
+        StartCoroutine(PlayItemPopup(newItem));
     }
 
-    private void PlayItemPopup(Item item)
+    private IEnumerator PlayItemPopup(Item item)
     {
         popUpParent.SetActive(true);
         //Sprite sprite = popUpParent.GetComponentInChildren<Image>().sprite;
         TMP_Text text = GameObject.Find("ItemText").GetComponent<TMP_Text>();
-        for (int i = 0; i < item.mods.Count; i++)
-        {
-            if (item.mods[i] != 0)
-            {
-                GameObject ModsParent = GameObject.Find("ListOfMods");
-                GameObject ModText = Instantiate(modsText, new Vector3(ModsParent.transform.position.x, ModsParent.transform.position.y, 0), Quaternion.identity);
-                ModText.transform.parent = ModsParent.transform;
-                if (item.mods[i] < 0)
-                {
-                    ModText.GetComponent<TMP_Text>().text = item.modsNames[i] + ": -" + item.mods[i].ToString();
-                }
-                else
-                {
-                    ModText.GetComponent<TMP_Text>().text = item.modsNames[i] + ": +" + item.mods[i].ToString();
-                }
+        //for (int i = 0; i < item.mods.Count; i++)
+        //{
+        //    if (item.mods[i] != 0)
+        //    {
+        //        GameObject ModsParent = GameObject.Find("ListOfMods");
+        //        GameObject ModText = Instantiate(modsText, new Vector3(ModsParent.transform.position.x, ModsParent.transform.position.y, 0), Quaternion.identity);
+        //        ModText.transform.parent = ModsParent.transform;
+        //        if (item.mods[i] < 0)
+        //        {
+        //            ModText.GetComponent<TMP_Text>().text = item.modsNames[i] + ": -" + item.mods[i].ToString();
+        //        }
+        //        else
+        //        {
+        //            ModText.GetComponent<TMP_Text>().text = item.modsNames[i] + ": +" + item.mods[i].ToString();
+        //        }
                 
-            }
+        //    }
             
-        }
+        //}
         //Debug.Log(sprite);
         //sprite = item.icon;
         //Debug.Log(sprite);
         
-        text.text = item.name;
+        text.text = "New Item! '" + item.name + "'";
+        yield return new WaitForSeconds(3f);
+        popUpParent.SetActive(false);
     }
 }
